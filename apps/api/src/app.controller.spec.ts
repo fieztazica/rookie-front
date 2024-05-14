ec965786b6d174386a74a3783520ff83ab824604
Dat Hoang <fiezt@outlook.com>
2024-05-14 10:15:51 +0700

    ci: fix zenstack enhance

--- apps/api/src/app.controller.spec.ts
+++ apps/api/src/app.controller.spec.ts
@@ -1,10 +1,6 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import configuration from './common/config/configuration';
-import { validate } from './common/config/env.validation';
 import { PrismaModule } from './common/database/prisma.module';
 import { PrismaService } from './common/database/prisma.service';
 import { CustomersModule } from './customers/customers.module';
@@ -14,28 +10,7 @@ describe('AppController', () => {
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({
-          isGlobal: true,
-          load: [configuration],
-          validate,
-          expandVariables: true,
-        }),
-        ThrottlerModule.forRootAsync({
-          imports: [ConfigModule],
-          inject: [ConfigService],
-          useFactory: (config: ConfigService) => ({
-            throttlers: [
-              {
-                ttl: config.get('THROTTLE_TTL'),
-                limit: config.get('THROTTLE_LIMIT'),
-              },
-            ],
-          }),
-        }),
-        PrismaModule,
-        CustomersModule,
-      ],
+      imports: [PrismaModule, CustomersModule],
       controllers: [AppController],
       providers: [AppService, PrismaService],
     }).compile();
