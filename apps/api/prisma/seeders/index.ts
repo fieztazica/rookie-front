c053b281ca640711a624f182a001dc6f2e560cad
Dat Hoang <fiezt@outlook.com>
2024-05-16 14:50:07 +0700

    feat(seed): add seeds for resources

---
+++ apps/api/prisma/seeders/index.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+export { seedAuthors } from './authors';
+export { seedCategories } from './categories';
+export { seedCustomers } from './customers';
+export { seedPublishers } from './publishers';
+export { seedProducts } from './products';
+export const prisma = new PrismaClient();
+export function getRandomFrom<T>(array: T[]): T {
+  return array[Math.floor(Math.random() * array.length)];
+}
+export function getRandomInRange(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+export function getCreateModels<T extends { id: string | number }>(
+  max: number = 2,
+  modelName: string,
+  data: T[],
+) {
+  const contents = new Array(getRandomInRange(1, max)).fill({
+    [modelName]: {
+      connect: { id: getRandomFrom(data).id },
+    },
+  });
+  console.log(
+    `[${modelName}]\tCreating ${contents.length} (maximum ${max}) from ${data.length} entities`,
+  );
+  return {
+    create: contents,
+  };
+}
