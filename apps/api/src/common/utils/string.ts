6533f1b2b11ce19ce774d8b1003b1b53ba4dc63b
Dat Hoang <fiezt@outlook.com>
2024-05-19 19:02:15 +0700

    feat(utils/string): add simple convertPluralToSingular

--- apps/api/src/common/utils/string.ts
+++ apps/api/src/common/utils/string.ts
@@ -2,3 +2,14 @@ export function convertCamelCaseToTitleCase(text: string) {
   const res = text.replace(/([A-Z])/g, ' $1').trim();
   return res.charAt(0).toUpperCase() + res.slice(1);
 }
+
+export function convertPluralToSingular(text: string) {
+  let result = text.toString();
+  if (result.endsWith('ies')) {
+    result = result.slice(0, -3) + 'y';
+  }
+  if (result.endsWith('s')) {
+    result = result.slice(0, -1);
+  }
+  return result;
+}
