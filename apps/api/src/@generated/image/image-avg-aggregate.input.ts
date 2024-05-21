80d7427ec2b99d2ea6341ad123f1bfa15ef18810
Dat Hoang <fiezt@outlook.com>
2024-05-21 10:28:42 +0700

    refactor: remove generated contents

--- apps/api/src/@generated/image/image-avg-aggregate.input.ts
+++
@@ -1,9 +0,0 @@
-import { Field } from '@nestjs/graphql';
-import { InputType } from '@nestjs/graphql';
-
-@InputType()
-export class ImageAvgAggregateInput {
-
-    @Field(() => Boolean, {nullable:true})
-    id?: true;
-}
