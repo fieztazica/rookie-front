56a4f7b8c47e65c3619075b3f0b26b8e246a9cc0
Dat Hoang <fiezt@outlook.com>
2024-05-29 21:05:36 +0700

    feat(cart): shorten cart service

--- apps/api/src/cart/cart.service.ts
+++ apps/api/src/cart/cart.service.ts
@@ -14,6 +14,40 @@ export class CartService {
   ) {}
 
   async get(customerId: string) {
+    return this.getOrSetCart(customerId);
+  }
+
+  async add(customerId: string, productId: string, amount: number = 1) {
+    let cart = await this.getOrSetCart(customerId);
+    if (amount < 1) return cart;
+    if (!cart.addItem) cart = new Cart(cart.items);
+    cart.addItem(productId, amount);
+    await this.redisService.hSetJson(this.name, customerId, cart);
+    return cart;
+  }
+
+  async remove(customerId: string, key: string, amount: number = 1) {
+    let cart = await this.getOrSetCart(customerId);
+    if (amount < 1) return cart;
+    if (!cart.removeItem) cart = new Cart(cart.items);
+    cart.removeItem(key, amount);
+    this.redisService.hSetJson(this.name, customerId, cart);
+    return cart;
+  }
+
+  async delete(customerId: string, key: string) {
+    let cart = await this.getOrSetCart(customerId);
+    if (!cart.deleteItem) cart = new Cart(cart.items);
+    cart.deleteItem(key);
+    this.redisService.hSetJson(this.name, customerId, cart);
+    return cart;
+  }
+
+  clear(customerId: string) {
+    return this.redisService.hDel(this.name, customerId);
+  }
+
+  async getOrSetCart(customerId: string) {
     const customer = await this.customersService.findOne(customerId);
     if (!customer) throw new BadRequestException();
     return this.redisService.hGetOrSetJson<Cart>(
@@ -21,53 +55,5 @@ export class CartService {
       customer.id,
       new Cart(),
     );
-  }
-
-  async add(customerId: string, productId: string, amount: number = 1) {
-    const customer = await this.customersService.findOne(customerId);
-    if (!customer) throw new BadRequestException();
-    let cart = await this.redisService.hGetOrSetJson<Cart>(
-      this.name,
-      customer.id,
-      new Cart(),
-    );
-    if (amount < 1) return cart;
-    if (!cart.addItem) cart = new Cart(cart.items);
-    cart.addItem(productId, amount);
-    await this.redisService.hSetJson(this.name, customer.id, cart);
-    return cart;
-  }
-
-  async remove(customerId: string, key: string, amount: number = 1) {
-    const customer = await this.customersService.findOne(customerId);
-    if (!customer) throw new BadRequestException();
-    let cart = await this.redisService.hGetOrSetJson<Cart>(
-      this.name,
-      customer.id,
-      new Cart(),
-    );
-    if (amount < 1) return cart;
-    if (!cart.removeItem) cart = new Cart(cart.items);
-    cart.removeItem(key, amount);
-    this.redisService.hSetJson(this.name, customer.id, cart);
-    return cart;
-  }
-
-  async delete(customerId: string, key: string) {
-    const customer = await this.customersService.findOne(customerId);
-    if (!customer) throw new BadRequestException();
-    let cart = await this.redisService.hGetOrSetJson<Cart>(
-      this.name,
-      customer.id,
-      new Cart(),
-    );
-    if (!cart.deleteItem) cart = new Cart(cart.items);
-    cart.deleteItem(key);
-    this.redisService.hSetJson(this.name, customer.id, cart);
-    return cart;
-  }
-
-  clear(customerId: string) {
-    return this.redisService.hDel(this.name, customerId);
   }
 }
