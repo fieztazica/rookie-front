cff43720ab484d554c05f124892b3102047c8379
Dat Hoang <fiezt@outlook.com>
2024-06-01 15:39:33 +0700

    feat(api): clean admin module imports

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -1,20 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthorsModule } from 'src/authors/authors.module';
-import { AuthorsService } from 'src/authors/authors.service';
 import { CategoriesModule } from 'src/categories/categories.module';
-import { CategoriesService } from 'src/categories/categories.service';
+import { ConfigsModule } from 'src/configs/configs.module';
 import { CustomersModule } from 'src/customers/customers.module';
-import { CustomersService } from 'src/customers/customers.service';
 import { FeedbacksModule } from 'src/feedbacks/feedbacks.module';
-import { FeedbacksService } from 'src/feedbacks/feedbacks.service';
 import { OrdersModule } from 'src/orders/orders.module';
-import { OrdersService } from 'src/orders/orders.service';
 import { ProductsModule } from 'src/products/products.module';
-import { ProductsService } from 'src/products/products.service';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
-import { ConfigsModule } from 'src/configs/configs.module';
-import { ConfigsService } from 'src/configs/configs.service';
 
 @Module({
   imports: [
@@ -27,15 +20,6 @@ import { ConfigsService } from 'src/configs/configs.service';
     ConfigsModule,
   ],
   controllers: [AdminController],
-  providers: [
-    AdminService,
-    AuthorsService,
-    CategoriesService,
-    CustomersService,
-    FeedbacksService,
-    OrdersService,
-    ProductsService,
-    ConfigsService,
-  ],
+  providers: [AdminService],
 })
 export class AdminModule {}
