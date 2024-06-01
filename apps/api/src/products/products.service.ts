a4cf352685030e18779ef410da4df9f93b186af2
Dat Hoang <fiezt@outlook.com>
2024-06-01 11:11:16 +0700

    feat(product); resolve ratings for product

--- apps/api/src/products/products.service.ts
+++ apps/api/src/products/products.service.ts
@@ -9,11 +9,15 @@ import { PrismaService } from '../common/database/prisma.service';
 import { CreateProductInput } from './dto/create-product.input';
 import { UpdateProductInput } from './dto/update-product.input';
 import { Prisma, Product } from '@prisma/client';
-
+import { FeedbacksService } from 'src/feedbacks/feedbacks.service';
+import { differenceInDays, differenceInSeconds } from 'date-fns';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class ProductsService {
   constructor(
     @Inject(ENHANCED_PRISMA) private readonly prisma: PrismaService,
+    private readonly feedbacksService: FeedbacksService,
+    private readonly configService: ConfigService,
   ) {}
   create(createProductInput: CreateProductInput): Promise<Product> {
     return this.prisma.product.create({ data: createProductInput });
@@ -43,6 +47,24 @@ export class ProductsService {
     return this.prisma.product.findUnique({
       where: { id, deleted: false },
     });
+  }
+
+  async calculateRatingsByProductId(productId: string, product?: Product) {
+    if (product) {
+      const testing: boolean = this.configService.get('testing');
+      const diff = testing
+        ? differenceInSeconds(new Date(), new Date(product.updatedAt))
+        : differenceInDays(new Date(), new Date(product.updatedAt));
+
+      if (diff < 7 && product.ratings > 0) return;
+    }
+    const ratings =
+      await this.feedbacksService.calculateRatingByProductId(productId);
+    await this.update(productId, {
+      id: productId,
+      ratings: ratings.averageRatings,
+    });
+    return ratings.averageRatings;
   }
 
   update(id: string, updateProductInput: UpdateProductInput): Promise<Product> {
