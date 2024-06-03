f7a41d92ff80a69e7f68e8f93e1bd268d63bb123
Dat Hoang <fiezt@outlook.com>
2024-06-03 11:50:35 +0700

    feat(admin/about): add plugins to tinymce

--- apps/api/assets/js/index.js
+++ apps/api/assets/js/index.js
@@ -1,6 +1,10 @@
 $(function () {
   tinymce.init({
     selector: 'textarea',
-    license_key: 'gpl',
+    plugins:
+      'powerpaste casechange searchreplace autolink directionality visualblocks visualchars image link media mediaembed codesample table charmap pagebreak nonbreaking anchor tableofcontents insertdatetime advlist lists checklist wordcount tinymcespellchecker editimage help formatpainter permanentpen charmap linkchecker emoticons advtable export autosave code importcss',
+    toolbar:
+      'undo redo print spellcheckdialog formatpainter | blocks fontfamily fontsize | bold italic underline forecolor backcolor | link image | alignleft aligncenter alignright alignjustify | code',
+    content_css: '/css/main.css',
   });
 });
