65657872c78b9a6fa8cb218b065b827b1239c97b
Dat Hoang <fiezt@outlook.com>
2024-06-03 23:12:38 +0700

    fix: typos

--- apps/web/src/components/product-list.tsx
+++ apps/web/src/components/product-list.tsx
@@ -1,9 +1,8 @@
 import Link from 'next/link';
-import { Product } from '../__generated__/graphql';
-import ProductCard from './product-card';
+import ProductCard, { type ReceivedProduct } from './product-card';
 
 type Props = {
-  products: Product[];
+  products: ReceivedProduct[];
 };
 
 function ProductList({ products }: Props) {
@@ -11,7 +10,7 @@ function ProductList({ products }: Props) {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {products.map((p) => (
         <Link key={`product_${p.id}`} href={`/product/${p.id}`}>
-          <ProductCard product={p} className={'h-full'}/>
+          <ProductCard product={p} className={'h-full'} />
         </Link>
       ))}
     </div>
