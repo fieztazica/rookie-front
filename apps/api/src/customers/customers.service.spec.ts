3aba021d32d6564877d1b2d389c26fbce187e4dd
Dat Hoang <fiezt@outlook.com>
2024-06-03 23:31:50 +0700

    fix: typos

--- apps/api/src/customers/customers.service.spec.ts
+++ apps/api/src/customers/customers.service.spec.ts
@@ -1,27 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CustomersService } from './customers.service';
 import { PrismaModule } from '../common/database/prisma.module';
-import { PrismaService } from '../common/database/prisma.service';
-import { prismaMock } from 'prisma/prisma.mock';
+import { CustomersService } from './customers.service';
 
 describe('CustomersService', () => {
   let service: CustomersService;
-  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [PrismaModule],
-      providers: [
-        CustomersService,
-        {
-          provide: PrismaService,
-          useValue: prismaMock,
-        },
-      ],
+      providers: [CustomersService],
     }).compile();
 
     service = module.get<CustomersService>(CustomersService);
-    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
