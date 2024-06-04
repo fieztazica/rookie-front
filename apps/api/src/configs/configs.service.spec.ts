9dc36d867ac9d3f3094fab06ae1377ecdf1f28b2
Dat Hoang <fiezt@outlook.com>
2024-06-04 12:55:48 +0700

    test: add product/config tests

--- apps/api/src/configs/configs.service.spec.ts
+++ apps/api/src/configs/configs.service.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigsService } from './configs.service';
 
-const mockConfigs = [
+export const mockConfigs = [
   { key: 'about', value: 'about', deleted: false },
   {
     key: 'API_KEY',
@@ -10,7 +10,7 @@ const mockConfigs = [
   },
 ];
 
-const paginatedConfigs = {
+export const paginatedConfigs = {
   data: [mockConfigs],
   meta: {
     total: mockConfigs.length,
@@ -22,20 +22,20 @@ const paginatedConfigs = {
   },
 };
 
-const oneConfig = mockConfigs[0];
-const updatedConfig = { ...oneConfig, value: 'updated' };
-const deletedConfig = { ...oneConfig, deleted: true };
+export const oneConfig = mockConfigs[0];
+export const updatedConfig = { ...oneConfig, value: 'updated' };
+export const deletedConfig = { ...oneConfig, deleted: true };
 
-const mockConfigsService = {
-  create: jest.fn().mockReturnValue(oneConfig),
-  findAll: jest.fn().mockReturnValue(mockConfigs),
-  paginatedFindAll: jest.fn().mockReturnValue(paginatedConfigs),
-  findOne: jest.fn().mockReturnValue(oneConfig),
-  get: jest.fn().mockReturnValue(oneConfig),
-  set: jest.fn().mockReturnValue(oneConfig),
-  getOrSet: jest.fn().mockReturnValue(oneConfig),
-  update: jest.fn().mockReturnValue(updatedConfig),
-  remove: jest.fn().mockReturnValue(deletedConfig),
+export const mockConfigsService = {
+  create: jest.fn().mockResolvedValue(oneConfig),
+  findAll: jest.fn().mockResolvedValue(mockConfigs),
+  paginatedFindAll: jest.fn().mockResolvedValue(paginatedConfigs),
+  findOne: jest.fn().mockResolvedValue(oneConfig),
+  get: jest.fn().mockResolvedValue(oneConfig),
+  set: jest.fn().mockResolvedValue(oneConfig),
+  getOrSet: jest.fn().mockResolvedValue(oneConfig),
+  update: jest.fn().mockResolvedValue(updatedConfig),
+  remove: jest.fn().mockResolvedValue(deletedConfig),
 };
 
 describe('ConfigsService', () => {
@@ -54,27 +54,27 @@ describe('ConfigsService', () => {
   });
 
   describe('get', () => {
-    it('should returns key value pair object', () => {
-      expect(service.get('about')).toEqual(oneConfig);
+    it('should returns key value pair object', async () => {
+      expect(await service.get('about')).toEqual(oneConfig);
     });
   });
 
   describe('set', () => {
-    it('should returns the setted key value pair object', () => {
-      expect(service.set('about', 'about')).toEqual(oneConfig);
+    it('should returns the setted key value pair object', async () => {
+      expect(await service.set('about', 'about')).toEqual(oneConfig);
     });
   });
 
   describe('getOrSet', () => {
-    it('should returns key value pair object if pair does not exist', () => {
-      expect(service.getOrSet('about', 'about')).toEqual(oneConfig);
+    it('should returns key value pair object if pair does not exist', async () => {
+      expect(await service.getOrSet('about', 'about')).toEqual(oneConfig);
     });
   });
 
   describe('update', () => {
-    it('should returns the updated key value pair object', () => {
+    it('should returns the updated key value pair object', async () => {
       expect(
-        service.update('about', {
+        await service.update('about', {
           key: 'about',
           value: 'updated',
         }),
@@ -83,32 +83,32 @@ describe('ConfigsService', () => {
   });
 
   describe('remove', () => {
-    it('should returns the deleted key value pair object', () => {
-      expect(service.remove('about')).toEqual(deletedConfig);
+    it('should returns the deleted key value pair object', async () => {
+      expect(await service.remove('about')).toEqual(deletedConfig);
     });
   });
 
   describe('findAll', () => {
-    it('should returns an array of key value pair objects', () => {
-      expect(service.findAll()).toEqual(mockConfigs);
+    it('should returns an array of key value pair objects', async () => {
+      expect(await service.findAll()).toEqual(mockConfigs);
     });
   });
 
   describe('paginatedFindAll', () => {
-    it('should returns the paginated with meta and data of key value pair objects', () => {
-      expect(service.paginatedFindAll()).toEqual(paginatedConfigs);
+    it('should returns the paginated with meta and data of key value pair objects', async () => {
+      expect(await service.paginatedFindAll()).toEqual(paginatedConfigs);
     });
   });
 
   describe('findOne', () => {
-    it('should returns the key value pair object', () => {
-      expect(service.findOne('key')).toEqual(oneConfig);
+    it('should returns the key value pair object', async () => {
+      expect(await service.findOne('key')).toEqual(oneConfig);
     });
   });
 
   describe('create', () => {
-    it('should returns the created key value pair object', () => {
-      expect(service.create({ key: 'about', value: 'about' })).toEqual(
+    it('should returns the created key value pair object', async () => {
+      expect(await service.create({ key: 'about', value: 'about' })).toEqual(
         oneConfig,
       );
     });
