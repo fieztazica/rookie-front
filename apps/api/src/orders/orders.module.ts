783190ffd2f7452ff6d18f33ba8df3b0b602d519
Dat Hoang <fiezt@outlook.com>
2024-06-04 20:44:53 +0700

    feat(api/order): make an order

--- apps/api/src/orders/orders.module.ts
+++ apps/api/src/orders/orders.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersResolver } from './orders.resolver';
+import { ProductsModule } from 'src/products/products.module';
 
 @Module({
+  imports: [ProductsModule],
   providers: [OrdersResolver, OrdersService],
   exports: [OrdersService],
 })
--- apps/api/src/orders/orders.service.ts
+++ apps/api/src/orders/orders.service.ts
@@ -9,14 +9,59 @@ import {
 import { PrismaService } from '../common/database/prisma.service';
 import { CreateOrderInput } from './dto/create-order.input';
 import { UpdateOrderInput } from './dto/update-order.input';
+import { CartItemInput } from 'src/cart/dto/cart-item.input';
+import { ProductsService } from 'src/products/products.service';
 
 @Injectable()
 export class OrdersService {
   constructor(
     @Inject(ENHANCED_PRISMA) private readonly prisma: PrismaService,
+    private readonly productsService: ProductsService,
   ) {}
-  create(createOrderInput: CreateOrderInput): Promise<Order> {
+  create(createOrderInput: CreateOrderInput) {
     return this.prisma.order.create({ data: createOrderInput });
+  }
+
+  async make(customerId: string, items: CartItemInput[]) {
+    const productIds = items.map((item) => item.key);
+    const foundProducts = await this.productsService.findAll({
+      where: {
+        id: {
+          in: productIds,
+        },
+      },
+    });
+
+    const priceMap = new Map<string, number>(
+      foundProducts.map((product) => [
+        product.id,
+        product.salePrice >= 0 ? product.salePrice : product.price,
+      ]),
+    );
+
+    const orderTotal = items.reduce((acc, item) => {
+      const price = priceMap.get(item.key);
+      if (!price) return acc;
+      return acc + price * item.value;
+    }, 0);
+
+    return this.prisma.order.create({
+      data: {
+        customerId,
+        orderItems: {
+          create: items.map((item) => ({
+            price: priceMap.get(item.key),
+            quantity: item.value,
+            product: {
+              connect: {
+                id: item.key,
+              },
+            },
+          })),
+        },
+        total: orderTotal,
+      },
+    });
   }
 
   findAll(
@@ -38,6 +83,17 @@ export class OrdersService {
 
   findOne(id: string): Promise<Order> {
     return this.prisma.order.findUnique({ where: { id, deleted: false } });
+  }
+
+  getOrderItems(orderId: string) {
+    return this.prisma.orderItem.findMany({
+      where: {
+        orderId,
+      },
+      include: {
+        product: true,
+      },
+    });
   }
 
   update(id: string, updateOrderInput: UpdateOrderInput): Promise<Order> {
